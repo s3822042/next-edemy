5ec52a58df202a68ceb236eaaea935d1f3845a29
s3822042 <s3822042@rmit.edu.vn>
2021-07-20 15:14:42 +0700

    setup mongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,49 @@
-require("dotenv").config;
-import express from "express";
-import cors from "cors";
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+import { readdirSync } from "fs";
+const csrf = require("csrf");
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@react-education.c14bu.mongodb.net/react-education?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+connectDB();
 
 const app = express();
 
-// middleware
+// middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
 app.use(morgan("dev"));
+
+// route
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+// app.use(csrfProtection);
+
+// app.get("/api/csrf-token", (req, res) => {
+//   res.json({ csrfToken: req.csrfToken() });
+// });
+// port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
