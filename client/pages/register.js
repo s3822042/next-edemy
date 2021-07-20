c528986fa70cf5f9d7f537831c38219a56963e36
s3822042 <s3822042@rmit.edu.vn>
2021-07-20 20:17:45 +0700

    add login and register component

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,72 +3,92 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { SyncOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import { Context } from "../context";
+import { useRouter } from "next/router";
+import user from "../../server/models/user";
 
 const Register = () => {
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user !== null) router.push("/");
+  }, [user]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // console.table({ name, email, password });
     try {
       setLoading(true);
       const { data } = await axios.post(`/api/register`, {
-        username,
+        name,
         email,
         password,
       });
-
-      toast.success("Register Success!");
-      setUsername("");
+      // console.log("REGISTER RESPONSE", data);
+      toast("Registration successful. Please login.");
+      setName("");
       setEmail("");
       setPassword("");
       setLoading(false);
     } catch (err) {
-      toast.error(err.response.data);
+      toast(err.response.data);
       setLoading(false);
     }
   };
+
   return (
     <>
-      <h1 className="jumbotron text-center square">Register</h1>
+      <h1 className="jumbotron text-center bg-primary square">Register</h1>
+
       <div className="container col-md-4 offset-md-4 pb-5">
         <form onSubmit={handleSubmit}>
           <input
             type="text"
             className="form-control mb-4 p-4"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="johndoe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
             required
           />
+
           <input
             type="email"
             className="form-control mb-4 p-4"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="johndoe@gmail.com"
+            placeholder="Enter email"
             required
           />
+
           <input
             type="password"
             className="form-control mb-4 p-4"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="password"
+            placeholder="Enter password"
             required
           />
+
           <button
             type="submit"
             className="btn btn-block btn-primary"
-            disabled={!username || !email || !password || loading}
+            disabled={!name || !email || !password || loading}
           >
-            {loading ? <SyncOutlined spin /> : "Register"}
+            {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
+
         <p className="text-center p-3">
-          Already registered ?{" "}
+          Already registered?{" "}
           <Link href="/login">
             <a>Login</a>
           </Link>
